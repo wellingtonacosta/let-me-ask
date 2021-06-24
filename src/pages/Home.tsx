a52cff9ec39fe2ce26612c78ee5ac3d095f1141c
Wellington Costa <wellingtonacosta09@gmail.com>
2021-06-24 13:48:02 -0300

    feat: auth and global scss autentication

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ import googleIconImg from '../assets/images/google-icon.svg'
 
 export function Home() {
   return (
-      <div>
+      <div id='page-auth'>
         <aside>
          <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
           <strong>Crie salas de Q&amp;A ao-vivo</strong>
